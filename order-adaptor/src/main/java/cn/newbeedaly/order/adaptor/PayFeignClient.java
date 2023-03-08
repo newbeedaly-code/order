ec76500ad8293e214e64f518e4a982daa9554eb0
newbeedaly <newbeedaly@163.com>
2023-03-08 21:00:21 +0800

    update

--- order-adaptor/src/main/java/cn/newbeedaly/order/adaptor/PayFeignClient.java
+++ order-adaptor/src/main/java/cn/newbeedaly/order/adaptor/PayFeignClient.java
@@ -4,7 +4,7 @@ import cn.newbeedaly.order.adaptor.config.DefaultFeignClientConfiguration;
 import cn.newbeedaly.pay.api.client.PayClient;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(url = "127.0.0.1:8000", name = "pay", contextId = "PayFeignClient",
+@FeignClient(name = "pay", contextId = "PayFeignClient",
         path = "/pay", configuration = DefaultFeignClientConfiguration.class)
 public interface PayFeignClient extends PayClient {
 
